5742d401bbe71c0cbf5f7b23f62e16cb9189dcc3
Piotr <boro2012@gmail.com>
2017-09-12 13:27:31 +0200

    Small refactor

--- get_most_occurent_letter.py
+++ get_most_occurent_letter.py
@@ -1,13 +1,9 @@
 import re
 
-def checkio(text):
+def check_letters(text):
 
     my_string = only_letters(text)
-
     max_value = get_higher_occurence(list_of_occurences(my_string))
-
-    #replace this for solution
-
     my_letters = check_equal_occurences(list_of_occurences(my_string), max_value)
 
     return get_firs_letter(my_letters)
@@ -21,15 +17,10 @@ def list_of_occurences(text):
     occurences = dict()
 
     for i in text:
-
         i = i.lower()
-
         if i not in occurences:
-
             occurences[i] = 1
-
         else:
-
             occurences[i] += 1    
 
     return occurences
@@ -41,13 +32,11 @@ def get_higher_occurence(dictionary):
 def check_equal_occurences(dictionary, maximum):
 
     equals = []
-
+	
     for letter, value in dictionary.items():
-
         if value == maximum:
-
             equals.append(letter)
-
+			
     return equals
 
 def get_firs_letter(letters):
@@ -56,23 +45,23 @@ def get_firs_letter(letters):
 
 if __name__ == '__main__':
 
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    #local testing
 
-    assert checkio("H.P.Lovecraft") == "a", "Start"
+    assert check_letters("H.P.Lovecraft") == "a", "Start"
 
-    assert checkio("How about this one?") == "o", "O is most wanted"
+    assert check_letters("How about this one?") == "o", "O is most wanted"
 
-    assert checkio("One") == "e", "All letter only once."
+    assert check_letters("One") == "e", "All letter only once."
 
-    assert checkio("Oops!") == "o", "Lower case."
+    assert check_letters("Oops!") == "o", "Lower case."
 
-    assert checkio("AAaooo!!!!") == "a", "Only letters."
+    assert check_letters("AAaooo!!!!") == "a", "Only letters."
 
-    assert checkio("abe") == "a", "The First."
+    assert check_letters("abe") == "a", "The First."
 
     print("Start the long test")
 
-    assert checkio("a" * 9000 + "b" * 1000) == "a", "Very long."
+    assert check_letters("a" * 9000 + "b" * 1000) == "a", "Very long."
 
     print("The local tests are done.")
 
